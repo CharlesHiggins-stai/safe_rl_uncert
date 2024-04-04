c2cd5c257cedddfaa194aa4fa00327feea746004
CharlesHiggins-stai <charles.higgins@kcl.ac.uk>
2024-04-04 07:40:04 +0000

    periodic results

--- scripts/point/script_tune.sh
+++ scripts/point/script_tune.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-for s in 0; do
-    for ut in 0.01 0.025 0.05 0.075 0.1 0.25 0.5 0.75 1; do
+for s in 0 10 20; do
+    for ut in 0.01 0.05 0.1 0.25 0.5 0.75 1 2; do
 
   ##############################
   # UNCERTAINTY QUANTIFICATION # 
   ##############################
   # Intervention with point critic (sparse cost)
-  # ./cppo.py --epochs 500 --ignore_unsafe_cost --intv_config intv/critic_no_smoothing.yaml -s $s \
-  #   --exp_name point_critic_intv_sparse_cost_UNCERTAINTY_${ut} --uncertainty --uncert_threshold $ut
+  ./cppo.py --epochs 500 --ignore_unsafe_cost --intv_config intv/critic_no_smoothing.yaml -s $s \
+    --exp_name point_critic_intv_sparse_cost_UNCERTAINTY_${ut} --uncertainty --uncert_threshold $ut
 
-  # # Intervention with point critic (shaped cost)
-  # ./cppo.py --epochs 500 --ignore_unsafe_cost --intv_config intv/critic_smoothing.yaml -s $s \
-  #   --exp_name point_critic_intv_shaped_cost_UNCERTAINTY_${ut} --uncertainty --uncert_threshold $ut
+  # Intervention with point critic (shaped cost)
+  ./cppo.py --epochs 500 --ignore_unsafe_cost --intv_config intv/critic_smoothing.yaml -s $s \
+    --exp_name point_critic_intv_shaped_cost_UNCERTAINTY_${ut} --uncertainty --uncert_threshold $ut
 
   # Intervention with true model and sparse cost
   ./cppo.py --epochs 500 --ignore_unsafe_cost --intv_config intv/sparse.yaml \
